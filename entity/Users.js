68b196df27a2913174aa06bc7db314229ac77871
ChrisLiu-Snyk <xian.liu@snyk.io>
2025-06-18 17:11:30 +0800

    Update Users.js

--- entity/Users.js
+++ entity/Users.js
@@ -17,15 +17,3 @@ module.exports = {
     }
   }
 };
-
-async function getUserUnsafe1(req, res) {
-  const userId = req.params.id; // User-supplied ID from URL
-  // DANGEROUS: Direct string concatenation
-  const query = `SELECT * FROM users WHERE id = ${userId}`;
-  try {
-    const user = await db.query(query); // Execute raw query
-    res.json(user);
-  } catch (error) {
-    res.status(500).send("Error fetching user");
-  }
-}
